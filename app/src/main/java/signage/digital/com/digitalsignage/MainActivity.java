b6121ddce3d4afc8b592a93c63fa54cd23360371
alexandrefett@gmail.com <tuneca2011>
2017-11-29 08:49:24 -0200

    versao 4

--- app/src/main/java/signage/digital/com/digitalsignage/MainActivity.java
+++ app/src/main/java/signage/digital/com/digitalsignage/MainActivity.java
@@ -3,10 +3,12 @@ package signage.digital.com.digitalsignage;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -20,9 +22,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener{
+import signage.digital.com.digitalsignage.adapter.FragmentAdapter;
+import signage.digital.com.digitalsignage.fragment.FragmentAdv;
+import signage.digital.com.digitalsignage.fragment.FragmentEvent;
+import signage.digital.com.digitalsignage.model.Conference;
+
+
+public class MainActivity extends AppCompatActivity implements FragmentEvent.OnCalendarChangeListener{
     private FirebaseAuth mAuth;
+    ViewPager viewPager;
+    FragmentAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +42,28 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
         mAuth = FirebaseAuth.getInstance();
 
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.play).setOnClickListener(this);
+    }
+
+
+    private void buildScreen(){
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        setContentView(R.layout.activity_fullscreen2);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        adapter = new FragmentAdapter(getSupportFragmentManager());
+        adapter.add(new FragmentAdv());
+        adapter.add(new FragmentEvent());
+        viewPager.setAdapter(adapter);
+
     }
 
     @Override
@@ -50,8 +82,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         boolean isSignedIn = (u != null);
         if (!isSignedIn) {
             signInAnonymously();
+        } else {
+            buildScreen();
         }
-
     }
 
     private void signInAnonymously() {
@@ -72,10 +105,11 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
     }
 
     @Override
-    public void onClick(View view) {
-        if(view.getId()==R.id.play) {
-            Intent intent = new Intent(this, PlayActivity.class);
-            startActivityForResult(intent, 2);
+    public void onCalendarChange(List<Conference> list) {
+        if(list.size()==0) {
+            viewPager.setCurrentItem(0);
+        } else {
+            viewPager.setCurrentItem(1);
         }
     }
 }
